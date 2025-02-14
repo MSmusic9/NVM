f6500e63d0f2ef0d3eb630abdcfe2f2a987bffc3
MSmusic9 <121720254+MSmusic9@users.noreply.github.com>
2025-02-14 20:14:43 +0300

    Update nvm.c

--- src/nvm.c
+++ src/nvm.c
@@ -3,23 +3,52 @@
 
 
 #define NVM_DISPATCH(OP) op_##OP: \
-NVM_OP_##OP; \
+SERVICE_##OP; \
+goto fetch;
+
+#define NVM_CDDISPATCH(OP) op_##OP: \
+SERVICE_CD_##OP; \
 goto fetch;
 
 
 
-void nvm_start(const nvm_opcode_t* code){
+void nvm_ramExec(const nvm_opcode_t* code){
   register nvm_opcode_t* base = code;
   // initialize opcode table and start loop
   goto init;
 
-do_first_instr:
-  goto *table[*code];
-
 fetch:
-  goto *table[*(++code)];
+  goto *table[*code];
 
 init:
   // start loop
-  goto do_first_instr
+  goto fetch;
+
+op_illegal:
+  printf("NVM: illegal instruction (halting)\n");
+  printf("At: RAM mode\n");
+  printf("\tPhysic address: %p\n", (void*)code);
+  printf("\tRelative address: &p\n", (void*)(code - base));
+  return;
 }
+
+
+
+void nvm_diskExec(FILE* handle){
+  goto init;
+
+fetch:
+  goto *table[fgetc(handle)];
+
+init:
+  goto fetch;
+
+op_illegal:
+  printf("NVM: illegal instruction (halting)\n");
+  printf("At: disk mode\n");
+  printf("\tPhysic address: %lu\n", ftell(handle));
+  printf("\tRelative address: &lu\n", ftell(handle));
+  return;
+}
+
+
